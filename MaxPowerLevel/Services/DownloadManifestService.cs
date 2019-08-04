e893b909070a2a448da1654164dd1274b2fe2a94
Andy Schott <andy@andyschott.com>
2019-08-04 15:34:09 -0400

    clean up usings

--- MaxPowerLevel/Services/DownloadManifestService.cs
+++ MaxPowerLevel/Services/DownloadManifestService.cs
@@ -6,7 +6,6 @@ using Destiny2;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace MaxPowerLevel.Services
 {

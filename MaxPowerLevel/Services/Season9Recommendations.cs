e34a0f9511a2bc7589194259ad81ca6045d187a7
Andy Schott <andy@andyschott.com>
2020-01-07 23:01:42 -0500

    add Sundial Legend pinnacle

--- MaxPowerLevel/Services/Season9Recommendations.cs
+++ MaxPowerLevel/Services/Season9Recommendations.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using Destiny2;
+using MaxPowerLevel.Models;
 
 namespace MaxPowerLevel.Services
 {
@@ -8,9 +11,28 @@ namespace MaxPowerLevel.Services
         protected override int HardCap => 970;
         protected override uint SeasonHash => 2007338097;
 
+        private const string SundialLegend = "The Sundial: Legend";
+
         public Season9Recommendations(IManifest manifest)
             : base(manifest)
         {
+        }
+
+        protected override IEnumerable<PinnacleActivity> CreatePinnacleActivities()
+        {
+            return base.CreatePinnacleActivities()
+                .Concat(new[]
+                {
+                    new PinnacleActivity(SundialLegend, new[]
+                    {
+                        new[]
+                        {
+                            ItemSlot.SlotHashes.Kinetic,
+                            ItemSlot.SlotHashes.Energy,
+                            ItemSlot.SlotHashes.Power
+                        }
+                    })
+                });
         }
     }
 }

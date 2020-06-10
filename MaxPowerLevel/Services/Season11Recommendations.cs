4a8cd8724bc64b26cc8a823c5ca1b6e962b892e6
Andy Schott <andy@andyschott.com>
2020-06-09 22:14:56 -0400

    add season 11 pinnacle activities

--- MaxPowerLevel/Services/Season11Recommendations.cs
+++ MaxPowerLevel/Services/Season11Recommendations.cs
@@ -1,10 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
 using Destiny2;
+using MaxPowerLevel.Models;
 using VendorEngrams;
 
 namespace MaxPowerLevel.Services
 {
     public class Season11Recommendations : Year3Recommendations
     {
+        private const string Prophecy = "Prophecy"; // New Dungeon
+        private const string DarkTimes = "Dark Times"; // Weekly for PE completions
+
         public Season11Recommendations(IManifest manifest, IVendorEngramsClient vendorEngrams, SeasonPass seasonPass)
             : base(manifest, vendorEngrams, seasonPass)
         {
@@ -17,5 +23,15 @@ namespace MaxPowerLevel.Services
         protected override int HardCap => 1060;
 
         protected override uint SeasonHash => 248573323;
+
+        protected override IEnumerable<PinnacleActivity> CreatePinnacleActivities()
+        {
+            return base.CreatePinnacleActivities()
+                .Concat(new[]
+                {
+                    new PinnacleActivity(Prophecy, new[] { _allSlots }),
+                    new PinnacleActivity(DarkTimes, new[] { _allSlots })
+                });
+        }
     }
 }

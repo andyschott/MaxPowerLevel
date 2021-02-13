af929961324e56f297284c5f4d6f23dd540d83e5
Max Ignatenko <gelraen.ua@gmail.com>
2021-02-13 00:26:55 +0000

    Add client-side mode toggle

--- MaxPowerLevel/wwwroot/js/site.js
+++ MaxPowerLevel/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function updateCSSFilename(id, filename) {
+    let el = $(document.getElementById(id));
+    let path_re = new RegExp("^((?:.*/)*)([^/?]*)(\\?.*)?$");
+    let url_parts = path_re.exec(el.attr('href'));
+    el.attr('href',
+        (url_parts[1] || '') + filename + (url_parts[3] || ''));
+}
+
+$('.switch-to-dark-mode').click(function() {
+    updateCSSFilename('bulma-css', 'bulma-dark.css');
+});
+
+$('.switch-to-light-mode').click(function() {
+    updateCSSFilename('bulma-css', 'bulma-default.css');
+});

d0c0ebd246d6be58e80e7488d98a36ec400f9efa
Andy Schott <andy@andyschott.com>
2021-08-24 21:45:08 -0400

    add Shattered Realm pinnacle

--- MaxPowerLevel/Services/YearFour/Season15.cs
+++ MaxPowerLevel/Services/YearFour/Season15.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using MaxPowerLevel.Models;
+
 namespace MaxPowerLevel.Services.YearFour
 {
     public class Season15 : AbstractYear4Season
@@ -9,5 +13,13 @@ namespace MaxPowerLevel.Services.YearFour
         public override int HardCap => 1330;
 
         public override uint SeasonHash => 2698636901;
+
+        public override IEnumerable<PinnacleActivity> CreatePinnacleActivities()
+        {
+            return base.CreatePinnacleActivities().Concat(new[]
+            {
+                new PinnacleActivity("Shattered Realm", new[] { AllSlots })
+            });
+        }
     }
 }

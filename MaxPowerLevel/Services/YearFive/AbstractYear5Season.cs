981978ef02055bc44a321dc8b44fe1530b5644fc
Andy Schott <andy@andyschott.com>
2022-02-25 11:48:20 -0500

    add pinnacles for Dares of Eternity, Vox Obscura, and Wellspring

--- MaxPowerLevel/Services/YearFive/AbstractYear5Season.cs
+++ MaxPowerLevel/Services/YearFive/AbstractYear5Season.cs
@@ -6,13 +6,19 @@ namespace MaxPowerLevel.Services.YearFive
     public abstract class AbstractYear5Season : AbstractSeason
     {
         protected static readonly PinnacleActivity _graspOfAvarice = new PinnacleActivity("Grasp of Avarice", new[] { AllSlots });
+        protected static readonly PinnacleActivity _daresOfEternity = new PinnacleActivity("Dares of Eternity Pinnacle Challenge", new[] { AllSlots });
+        protected static readonly PinnacleActivity _voxObscura = new PinnacleActivity("Vox Obscura", new[] { AllSlots });
+        protected static readonly PinnacleActivity _wellspring = new PinnacleActivity("Wellspring Pinnacle Challenge", new[] { AllSlots });
 
         protected override IEnumerable<PinnacleActivity> CreatePinnacleActivities()
         {
             return new[]
             {
                 NightfallScore,
-                _graspOfAvarice
+                _graspOfAvarice,
+                _daresOfEternity,
+                _voxObscura,
+                _wellspring
             };
         }
     }
